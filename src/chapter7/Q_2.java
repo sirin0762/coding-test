7778a175c70436df1874cde7ee44a525b4a5924b
sirin0762 <tlsdydwls123@gmail.com>
2023-08-18 20:26:57 +0900

    Chapter 7.2 풀이(성공)

--- src/chapter7/Q_2.java
+++ src/chapter7/Q_2.java
@@ -1,2 +1,24 @@
-package chapter7;public class Q_2 {
+package chapter7;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q_2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+
 }
