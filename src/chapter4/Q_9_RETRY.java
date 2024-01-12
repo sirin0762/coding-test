218cdc3c32779e3da7b83a92aa93201c263120a7
sirin0762 <tlsdydwls123@gmail.com>
2024-01-12 19:26:29 +0900

    FEAT : Chapter 4-9 문제 되풀이(성공)

---
+++ src/chapter4/Q_9_RETRY.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class Q_9_RETRY {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean solution(String s) {
+        return s.matches("[0-9]{4}|[0-9]]{6}");
+    }
+
+}
