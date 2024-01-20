dd7fe68a1b3d76f0de9962a23d8fc8dd3d9d860c
sirin0762 <tlsdydwls123@gmail.com>
2024-01-20 17:44:33 +0900

    FEAT : Chapter 9-3 문제 되풀이(성공)

---
+++ src/chapter9/Q_3_RETRY.java
@@ -0,0 +1,25 @@
+package chapter9;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q_3_RETRY {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String before, String after) {
+        char[] beforeChars = before.toCharArray();
+        char[] afterChars = after.toCharArray();
+
+        Arrays.sort(beforeChars);
+        Arrays.sort(afterChars);
+        for (int i = 0; i < afterChars.length; i++) {
+            if (beforeChars[i] != afterChars[i]) return 0
+        }
+        return 1;
+    }
+
+}
